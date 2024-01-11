2e5de092b04c53d16f417fc9b65cb3a625e51e76
NinaIsaeva <ninaisaeva0919@gmail.com>
2024-01-11 20:34:45 +0100

    HW 08.01.24

--- src/main/java/de/telran/homeWorks/year2024/homeWork8_01_24/UtilOperation.java
+++ src/main/java/de/telran/homeWorks/year2024/homeWork8_01_24/UtilOperation.java
@@ -4,19 +4,16 @@ public class UtilOperation <T extends Number> {
     private T type;
 
     public T add(T arg1, T arg2) {
-        if (arg1 == null || arg2 == null)
-            return null;
         if (arg1 instanceof Integer && arg2 instanceof Integer) {
-            return (T) (Integer)((Integer)arg1+(Integer)arg2);
+            return (T) (Integer) ((Integer) arg1 + (Integer) arg2);
         } else if (arg1 instanceof Double && arg2 instanceof Double) {
             return (T) (Double) ((Double) arg1 + (Double) arg2);
         } else {
             throw new IllegalArgumentException("Тип не распознан");
         }
     }
+
     public T sub(T arg1, T arg2) {
-        if (arg1 == null || arg2 == null)
-            return null;
         if (arg1 instanceof Integer && arg2 instanceof Integer) {
             return (T) (Integer) ((Integer) arg1 - (Integer) arg2);
         } else if (arg1 instanceof Double && arg2 instanceof Double) {
@@ -25,6 +22,7 @@ public class UtilOperation <T extends Number> {
             throw new IllegalArgumentException("Тип не распознан");
         }
     }
+
     public T mult(T arg1, T arg2) {
         if (arg1 == null || arg2 == null)
             return null;
@@ -36,6 +34,7 @@ public class UtilOperation <T extends Number> {
             throw new IllegalArgumentException("Тип не распознан");
         }
     }
+
     public T div(T arg1, T arg2) {
         if (arg1 == null || arg2 == null)
             return null;
@@ -47,6 +46,7 @@ public class UtilOperation <T extends Number> {
             throw new IllegalArgumentException("Тип не распознан");
         }
     }
+
     public T sqr(T arg1) {
         if (arg1 instanceof Integer) {
             return (T) (Integer) ((Integer) arg1 * (Integer) arg1);
